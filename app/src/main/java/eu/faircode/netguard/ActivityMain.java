eac51e9e454812e4fe23dd25ed6e9a05c82fff54
M66B <marcel@faircode.eu>
2015-10-26 14:55:09 +0100

    Removed refresh menu

--- app/src/main/java/eu/faircode/netguard/ActivityMain.java
+++ app/src/main/java/eu/faircode/netguard/ActivityMain.java
@@ -236,10 +236,6 @@ public class ActivityMain extends AppCompatActivity implements SharedPreferences
                     Log.w(TAG, settings + " not available");
                 return true;
 
-            case R.id.menu_refresh:
-                fillApplicationList();
-                return true;
-
             case R.id.menu_whitelist_wifi:
                 prefs.edit().putBoolean("whitelist_wifi", !prefs.getBoolean("whitelist_wifi", true)).apply();
                 fillApplicationList();
