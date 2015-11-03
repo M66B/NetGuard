215e6b3c81aeb6634cb3c439f71dd96e1364d009
M66B <marcel@faircode.eu>
2015-11-03 17:42:49 +0100

    Remove settings on uninstalling application

--- app/src/main/java/eu/faircode/netguard/Receiver.java
+++ app/src/main/java/eu/faircode/netguard/Receiver.java
@@ -3,6 +3,7 @@ package eu.faircode.netguard;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.VpnService;
 import android.util.Log;
 
@@ -14,8 +15,21 @@ public class Receiver extends BroadcastReceiver {
         Log.i(TAG, "Received " + intent);
         Util.logExtras(TAG, intent);
 
-        // Start service
-        if (VpnService.prepare(context) == null)
-            SinkholeService.start(context);
+        if (Intent.ACTION_PACKAGE_REMOVED.equals(intent.getAction())) {
+            // Remove settings
+            if (intent.getBooleanExtra(Intent.EXTRA_DATA_REMOVED, false)) {
+                String packageName = intent.getData().getSchemeSpecificPart();
+                Log.i(TAG, "Deleting settings package=" + packageName);
+                context.getSharedPreferences("wifi", Context.MODE_PRIVATE).edit().remove(packageName).apply();
+                context.getSharedPreferences("other", Context.MODE_PRIVATE).edit().remove(packageName).apply();
+                context.getSharedPreferences("unused", Context.MODE_PRIVATE).edit().remove(packageName).apply();
+                context.getSharedPreferences("roaming", Context.MODE_PRIVATE).edit().remove(packageName).apply();
+            }
+
+        } else {
+            // Start service
+            if (VpnService.prepare(context) == null)
+                SinkholeService.start(context);
+        }
     }
 }
