d3a4ec0aaaa81dbcec4dde8d98d387d8620bb1a5
M66B <marcel@faircode.eu>
2015-11-08 19:30:06 +0100

    Cleanup

--- app/src/main/java/eu/faircode/netguard/Util.java
+++ app/src/main/java/eu/faircode/netguard/Util.java
@@ -152,26 +152,6 @@ public class Util {
         });
     }
 
-    public static Bitmap drawableToBitmap(Drawable drawable) {
-        if (drawable instanceof BitmapDrawable) {
-            BitmapDrawable bitmapDrawable = (BitmapDrawable) drawable;
-            if (bitmapDrawable.getBitmap() != null)
-                return bitmapDrawable.getBitmap();
-        }
-
-        Bitmap bitmap;
-        if (drawable.getIntrinsicWidth() <= 0 || drawable.getIntrinsicHeight() <= 0)
-            bitmap = Bitmap.createBitmap(1, 1, Bitmap.Config.ARGB_8888);
-        else
-            bitmap = Bitmap.createBitmap(drawable.getIntrinsicWidth(), drawable.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
-
-        Canvas canvas = new Canvas(bitmap);
-        drawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
-        drawable.draw(canvas);
-
-        return bitmap;
-    }
-
     public static boolean hasValidFingerprint(String tag, Context context) {
         try {
             PackageManager pm = context.getPackageManager();
