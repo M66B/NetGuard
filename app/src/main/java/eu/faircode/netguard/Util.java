3d22a70012afb1ba6e6a8b58a7d0696b3f448c57
M66B <marcel@faircode.eu>
2015-11-09 10:02:18 +0100

    Cleanup

--- app/src/main/java/eu/faircode/netguard/Util.java
+++ app/src/main/java/eu/faircode/netguard/Util.java
@@ -143,36 +143,6 @@ public class Util {
         }
     }
 
-    public static void sendCrashReport(Throwable ex, Context context) {
-        ApplicationErrorReport report = new ApplicationErrorReport();
-        report.packageName = report.processName = context.getPackageName();
-        report.time = System.currentTimeMillis();
-        report.type = ApplicationErrorReport.TYPE_CRASH;
-        report.systemApp = false;
-
-        ApplicationErrorReport.CrashInfo crash = new ApplicationErrorReport.CrashInfo();
-        crash.exceptionClassName = ex.getClass().getSimpleName();
-        crash.exceptionMessage = ex.getMessage();
-
-        StringWriter writer = new StringWriter();
-        PrintWriter printer = new PrintWriter(writer);
-        ex.printStackTrace(printer);
-
-        crash.stackTrace = writer.toString();
-
-        StackTraceElement stack = ex.getStackTrace()[0];
-        crash.throwClassName = stack.getClassName();
-        crash.throwFileName = stack.getFileName();
-        crash.throwLineNumber = stack.getLineNumber();
-        crash.throwMethodName = stack.getMethodName();
-
-        report.crashInfo = crash;
-
-        Intent intent = new Intent(Intent.ACTION_APP_ERROR);
-        intent.putExtra(Intent.EXTRA_BUG_REPORT, report);
-        context.startActivity(intent);
-    }
-
     public static void sendLogcat(final String tag, final Context context) {
         AsyncTask task = new AsyncTask<Object, Object, Intent>() {
             @Override
