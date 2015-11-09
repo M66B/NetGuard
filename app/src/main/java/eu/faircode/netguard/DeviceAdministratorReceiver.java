c056d7db93d9866e46174e26a0bb0d61df528baa
M66B <marcel@faircode.eu>
2015-11-09 11:18:37 +0100

    Device admin to prevent uninstall
    Refs #92

---
+++ app/src/main/java/eu/faircode/netguard/DeviceAdministratorReceiver.java
@@ -0,0 +1,41 @@
+package eu.faircode.netguard;
+
+/*
+    This file is part of NetGuard.
+
+    NetGuard is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    NetGuard is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with NetGuard.  If not, see <http://www.gnu.org/licenses/>.
+
+    Copyright 2015 by Marcel Bokhorst (M66B)
+*/
+
+import android.app.admin.DeviceAdminReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+public class DeviceAdministratorReceiver extends DeviceAdminReceiver {
+    private static final String TAG = "NetGuard.Admin";
+
+    @Override
+    public void onEnabled(Context context, Intent intent) {
+        super.onEnabled(context, intent);
+        Log.i(TAG, "Device admin enabled");
+    }
+
+    @Override
+    public void onDisabled(Context context, Intent intent) {
+        super.onDisabled(context, intent);
+        Log.i(TAG, "Device admin disabled");
+    }
+}
