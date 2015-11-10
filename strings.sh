b80b96acfea352510996d801b22db75b1ba99522
M66B <marcel@faircode.eu>
2015-11-10 16:35:31 +0100

    Updated texts

--- strings.sh
+++ strings.sh
@@ -3,4 +3,4 @@
 #\ \ \ \ <string name=\"msg_admin\">Prevent NetGuard from being uninstalled</string>'
 
 #grep -RIl "\<string name=\"setting_system" app/src/main/res | xargs sed -i -e '/setting_system/d'
-grep -RIl "\<string name=\"msg_admin" app/src/main/res | xargs sed -i -e 's/Prevent NetGuard from being uninstalled/Prevent from being uninstalled/g'
+grep -RIl "\<string name=\"summary_credentials" app/src/main/res | xargs sed -i -e 's/Prevent from being uninstalled/Prevent NetGuard from being uninstalled/g'
