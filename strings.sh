47a06bbf50bbd13cc5bf9a4f5068e221ba1294c5
M66B <marcel@faircode.eu>
2015-11-12 16:13:33 +0100

    Updated text

--- strings.sh
+++ strings.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-grep -RIl "\<string name=\"msg_try" app/src/main/res | xargs sed -i -e '/msg_try/a \
-\ \ \ \ <string name=\"msg_voluntary\">Donations are completely voluntary and do not unlock any feature. Donations are meant as a way to show your appreciation for the work done.</string>'
 
-#grep -RIl "\<string name=\"msg_admin" app/src/main/res | xargs sed -i -e '/msg_admin/d'
+grep -RIl "\<string name=\"msg_vpn" app/src/main/res | xargs sed -i -e '/msg_vpn/d'
+grep -RIl "\<string name=\"msg_completed" app/src/main/res | xargs sed -i -e '/msg_completed/a\
+\ \ \ \ <string name=\"msg_vpn\">NetGuard uses a local VPN as a sinkhole to block internet traffic.\
+For this reason, please allow a VPN connection in the next dialog.\
+Since NetGuard has no internet permission, you know your internet traffic is not being sent anywhere.</string>'
+
 #grep -RIl "\<string name=\"summary_credentials" app/src/main/res | xargs sed -i -e 's/Prevent from being uninstalled/Prevent NetGuard from being uninstalled/g'
