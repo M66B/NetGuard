bf170b265d49ca4ab26dcd31364843d5d938d084
M66B <marcel@faircode.eu>
2015-11-13 08:07:33 +0100

    New text, use scroll views for small screens

--- strings.sh
+++ strings.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-grep -RIl "\<string name=\"msg_vpn" app/src/main/res | xargs sed -i -e '/msg_vpn/d'
-grep -RIl "\<string name=\"msg_completed" app/src/main/res | xargs sed -i -e '/msg_completed/a\
-\ \ \ \ <string name=\"msg_vpn\">NetGuard uses a local VPN as a sinkhole to block internet traffic.\
-For this reason, please allow a VPN connection in the next dialog.\
-Since NetGuard has no internet permission, you know your internet traffic is not being sent anywhere.</string>'
+#grep -RIl "\<string name=\"msg_vpn" app/src/main/res | xargs sed -i -e '/msg_vpn/d'
+grep -RIl "\<string name=\"msg_voluntary" app/src/main/res | xargs sed -i -e '/msg_voluntary/a\
+\ \ \ \ <string name="msg_dimming">If you cannot press OK in the next dialog, another (screen dimming) application is likely manipulating the screen.</string>'
 
 #grep -RIl "\<string name=\"summary_credentials" app/src/main/res | xargs sed -i -e 's/Prevent from being uninstalled/Prevent NetGuard from being uninstalled/g'
