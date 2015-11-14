2a741e26e0d82e778462266026a8046a4abcbf91
M66B <marcel@faircode.eu>
2015-11-14 08:51:20 +0100

    Updated texts
    Closes #140

--- strings.sh
+++ strings.sh
@@ -2,9 +2,10 @@
 
 #grep -RIl "\<string name=\"msg_vpn" app/src/main/res | xargs sed -i -e '/msg_vpn/d'
 
-grep -RIl "\<string name=\"title_roaming" app/src/main/res | xargs sed -i -e '/title_roaming/a\
-\ \ \ \ <string name="title_internet">Has no internet access</string>'
-grep -RIl "\<string name=\"title_roaming" app/src/main/res | xargs sed -i -e '/title_roaming/a\
-\ \ \ \ <string name="title_disabled">Is disabled</string>'
+#grep -RIl "\<string name=\"title_roaming" app/src/main/res | xargs sed -i -e '/title_roaming/a\
+#\ \ \ \ <string name="title_internet">Has no internet access</string>'
+#grep -RIl "\<string name=\"title_roaming" app/src/main/res | xargs sed -i -e '/title_roaming/a\
+#\ \ \ \ <string name="title_disabled">Is disabled</string>'
 
-#grep -RIl "\<string name=\"summary_credentials" app/src/main/res | xargs sed -i -e 's/Prevent from being uninstalled/Prevent NetGuard from being uninstalled/g'
+grep -RIl "\<string name=\"title_disabled" app/src/main/res | xargs sed -i -e 's/Is disabled/is disabled/g'
+grep -RIl "\<string name=\"title_internet" app/src/main/res | xargs sed -i -e 's/Has no internet access/has no internet permission/g'
