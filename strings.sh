152f75e70c4745f00c828ec195f56f494eeaa7ac
M66B <marcel@faircode.eu>
2015-11-15 09:04:16 +0100

    Updated text

--- strings.sh
+++ strings.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
-#grep -RIl "\<string name=\"msg_vpn" app/src/main/res | xargs sed -i -e '/msg_vpn/d'
-
-#grep -RIl "\<string name=\"title_roaming" app/src/main/res | xargs sed -i -e '/title_roaming/a\
-#\ \ \ \ <string name="title_internet">Has no internet access</string>'
-#grep -RIl "\<string name=\"title_roaming" app/src/main/res | xargs sed -i -e '/title_roaming/a\
-#\ \ \ \ <string name="title_disabled">Is disabled</string>'
+grep -RIl "\<string name=\"msg_revoked" app/src/main/res | xargs sed -i -e '/msg_revoked/d'
+grep -RIl "\<string name=\"msg_disabled" app/src/main/res | xargs sed -i -e '/msg_disabled/a\
+\ \ \ \ <string name="msg_revoked">NetGuard has been disabled, likely by using another VPN based application</string>'
 
 grep -RIl "\<string name=\"title_disabled" app/src/main/res | xargs sed -i -e 's/Is disabled/is disabled/g'
 grep -RIl "\<string name=\"title_internet" app/src/main/res | xargs sed -i -e 's/Has no internet access/has no internet permission/g'
