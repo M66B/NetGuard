7cfa70c2ac09c871a026b3dfaef995c0accb7c64
M66B <marcel@faircode.eu>
2015-11-17 15:12:33 +0100

    Updated FAQ
    Refs #164

--- strings.sh
+++ strings.sh
@@ -1,17 +1,12 @@
 #!/bin/bash
 
-#grep -RIl "\<string name=\"setting_metered" app/src/main/res | xargs sed -i -e '/setting_metered/d'
+grep -RIl "\<string name=\"setting_metered" app/src/main/res | xargs sed -i -e '/setting_metered/d'
+grep -RIl "\<string name=\"setting_whitelist_roaming" app/src/main/res | xargs sed -i -e '/setting_whitelist_roaming/a\
+\ \ \ \ <string name="setting_metered">Handle metered WiFi networks</string>'
 
-#grep -RIl "\<string name=\"menu_invite" app/src/main/res | xargs sed -i -e '/menu_invite/a\
-#\ \ \ \ <string name="menu_faq">FAQ</string>'
-
-#grep -RIl "\<string name=\"msg_sure" app/src/main/res | xargs sed -i -e '/msg_sure/a\
-#\ \ \ \ <string name="msg_faq">Did you check the FAQ?</string>'
-
-grep -RIl "\<string name=\"msg_faq" app/src/main/res | xargs sed -i -e '/msg_faq/a\
-\ \ \ \ <string name="msg_no">No</string>'
-grep -RIl "\<string name=\"msg_faq" app/src/main/res | xargs sed -i -e '/msg_faq/a\
-\ \ \ \ <string name="msg_yes">Yes</string>'
+grep -RIl "\<string name=\"summary_metered" app/src/main/res | xargs sed -i -e '/summary_metered/d'
+grep -RIl "\<string name=\"summary_system" app/src/main/res | xargs sed -i -e '/summary_system/a\
+\ \ \ \ <string name="summary_metered">Apply mobile network rules to metered (paid, tethered) WiFi networks</string>'
 
 #grep -RIl "\<string name=\"title_disabled" app/src/main/res | xargs sed -i -e 's/Is disabled/is disabled/g'
 #grep -RIl "\<string name=\"title_internet" app/src/main/res | xargs sed -i -e 's/Has no internet access/has no internet permission/g'
