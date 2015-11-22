6c51a0be3c76e043cb3958543c9a663fe82b9d4e
M66B <marcel@faircode.eu>
2015-11-22 08:53:23 +0100

    Shorten texts

--- strings.sh
+++ strings.sh
@@ -6,11 +6,11 @@
 #\ \ \ \ <string name="title_screen_wifi">Allow Wi-Fi when screen is on</string>'
 #grep -RIl "\<string name=\"title_using" app/src/main/res | xargs sed -i -e '/title_using/d'
 
-grep -RIl "\<string name=\"setting_unused" app/src/main/res | xargs sed -i -e '/setting_unused/a\
-\ \ \ \ <string name="setting_screen_other">Default allow mobile when screen is on</string>'
-grep -RIl "\<string name=\"setting_unused" app/src/main/res | xargs sed -i -e '/setting_unused/a\
-\ \ \ \ <string name="setting_screen_wifi">Default allow Wi-Fi when screen is on</string>'
-grep -RIl "\<string name=\"setting_unused" app/src/main/res | xargs sed -i -e '/setting_unused/d'
+#grep -RIl "\<string name=\"setting_unused" app/src/main/res | xargs sed -i -e '/setting_unused/a\
+#\ \ \ \ <string name="setting_screen_other">Default allow mobile when screen is on</string>'
+#grep -RIl "\<string name=\"setting_unused" app/src/main/res | xargs sed -i -e '/setting_unused/a\
+#\ \ \ \ <string name="setting_screen_wifi">Default allow Wi-Fi when screen is on</string>'
+#grep -RIl "\<string name=\"setting_unused" app/src/main/res | xargs sed -i -e '/setting_unused/d'
 
-#grep -RIl "\<string name=\"title_disabled" app/src/main/res | xargs sed -i -e 's/Is disabled/is disabled/g'
-#grep -RIl "\<string name=\"title_internet" app/src/main/res | xargs sed -i -e 's/Has no internet access/has no internet permission/g'
+grep -RIl "\<string name=\"setting_screen_wifi" app/src/main/res | xargs sed -i -e 's/Default allow Wi-Fi when screen is on/Default allow Wi-Fi when screen on/g'
+grep -RIl "\<string name=\"setting_screen_other" app/src/main/res | xargs sed -i -e 's/Default allow mobile when screen is on/Default allow mobile when screen on/g'
