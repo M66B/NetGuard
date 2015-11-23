ac637888e5ddfa6dc0df97e838d24195782dac41
M66B <marcel@faircode.eu>
2015-11-23 10:58:22 +0100

    Prevent bug reporting from crashing NetGuard

--- app/src/main/java/eu/faircode/netguard/Util.java
+++ app/src/main/java/eu/faircode/netguard/Util.java
@@ -146,37 +146,41 @@ public class Util {
     }
 
     public static void sendCrashReport(Throwable ex, final Context context) {
-        ApplicationErrorReport report = new ApplicationErrorReport();
-        report.packageName = report.processName = context.getPackageName();
-        report.time = System.currentTimeMillis();
-        report.type = ApplicationErrorReport.TYPE_CRASH;
-        report.systemApp = false;
+        try {
+            ApplicationErrorReport report = new ApplicationErrorReport();
+            report.packageName = report.processName = context.getPackageName();
+            report.time = System.currentTimeMillis();
+            report.type = ApplicationErrorReport.TYPE_CRASH;
+            report.systemApp = false;
 
-        ApplicationErrorReport.CrashInfo crash = new ApplicationErrorReport.CrashInfo();
-        crash.exceptionClassName = ex.getClass().getSimpleName();
-        crash.exceptionMessage = ex.getMessage();
+            ApplicationErrorReport.CrashInfo crash = new ApplicationErrorReport.CrashInfo();
+            crash.exceptionClassName = ex.getClass().getSimpleName();
+            crash.exceptionMessage = ex.getMessage();
 
-        StringWriter writer = new StringWriter();
-        PrintWriter printer = new PrintWriter(writer);
-        ex.printStackTrace(printer);
+            StringWriter writer = new StringWriter();
+            PrintWriter printer = new PrintWriter(writer);
+            ex.printStackTrace(printer);
 
-        crash.stackTrace = writer.toString();
+            crash.stackTrace = writer.toString();
 
-        StackTraceElement stack = ex.getStackTrace()[0];
-        crash.throwClassName = stack.getClassName();
-        crash.throwFileName = stack.getFileName();
-        crash.throwLineNumber = stack.getLineNumber();
-        crash.throwMethodName = stack.getMethodName();
+            StackTraceElement stack = ex.getStackTrace()[0];
+            crash.throwClassName = stack.getClassName();
+            crash.throwFileName = stack.getFileName();
+            crash.throwLineNumber = stack.getLineNumber();
+            crash.throwMethodName = stack.getMethodName();
 
-        report.crashInfo = crash;
+            report.crashInfo = crash;
 
-        final Intent bug = new Intent(Intent.ACTION_APP_ERROR);
-        bug.putExtra(Intent.EXTRA_BUG_REPORT, report);
-        bug.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        if (bug.resolveActivity(context.getPackageManager()) == null)
-            sendLogcat(ex.toString() + "\n" + Log.getStackTraceString(ex), context);
-        else
-            context.startActivity(bug);
+            final Intent bug = new Intent(Intent.ACTION_APP_ERROR);
+            bug.putExtra(Intent.EXTRA_BUG_REPORT, report);
+            bug.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            if (bug.resolveActivity(context.getPackageManager()) == null)
+                sendLogcat(ex.toString() + "\n" + Log.getStackTraceString(ex), context);
+            else
+                context.startActivity(bug);
+        } catch (Throwable exex) {
+            Log.e(TAG, exex.toString() + "\n" + Log.getStackTraceString(exex));
+        }
     }
 
     public static void sendLogcat(final String message, final Context context) {
