aa313166f3bcc1583d9c557b2f1e78ec1460f8d7
M66B <marcel@faircode.eu>
2015-12-01 08:35:13 +0100

    Categorize settings

--- strings.sh
+++ strings.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
-grep -RIl "\<string name=\"msg_terms" app/src/main/res | xargs sed -i -e '/msg_terms/d'
-grep -RIl "\<string name=\"msg_voluntary" app/src/main/res | xargs sed -i -e '/msg_voluntary/a\
-\ \ <string name="msg_terms">By donating you agree to the <a href="http://www.netguard.me/#terms">terms &amp; conditions</a></string>'
+#grep -RIl "\<string name=\"setting_import" app/src/main/res | xargs sed -i -e '/setting_import/a\
+#\ \ <string name="setting_backup">Backup</string>'
+#grep -RIl "\<string name=\"setting_import" app/src/main/res | xargs sed -i -e '/setting_import/a\
+#\ \ <string name="setting_options">Options</string>'
+#grep -RIl "\<string name=\"setting_import" app/src/main/res | xargs sed -i -e '/setting_import/a\
+#\ \ <string name="setting_defaults">Defaults</string>'
+
+grep -RIl "\<string name=\"summary_national_roaming" app/src/main/res | xargs sed -i -e '/summary_national_roaming/d'
+grep -RIl "\<string name=\"summary_metered" app/src/main/res | xargs sed -i -e '/summary_metered/a\
+\ \ <string name="summary_national_roaming">Do not apply the roaming rules when the SIM and mobile network country are the same</string>'
 
 #grep -RIl "\<string name=\"setting_screen_wifi" app/src/main/res | xargs sed -i -e 's/xxx/yyy/g'
