4c0eb31e6cdadc4570a25c2c320048b491136dac
M66B <marcel@faircode.eu>
2016-02-01 14:23:58 +0100

    What a horrible Android bug
    Refs #243

---
+++ app/src/main/java/eu/faircode/netguard/SwitchPreference.java
@@ -0,0 +1,39 @@
+package eu.faircode.netguard;
+
+/*
+    This file is part of NetGuard.
+
+    NetGuard is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    NetGuard is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with NetGuard.  If not, see <http://www.gnu.org/licenses/>.
+
+    Copyright 2015-2016 by Marcel Bokhorst (M66B)
+*/
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+// https://code.google.com/p/android/issues/detail?id=26194
+
+public class SwitchPreference extends android.preference.SwitchPreference {
+    public SwitchPreference(Context context) {
+        this(context, null);
+    }
+
+    public SwitchPreference(Context context, AttributeSet attrs) {
+        this(context, attrs, android.R.attr.switchPreferenceStyle);
+    }
+
+    public SwitchPreference(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+}
