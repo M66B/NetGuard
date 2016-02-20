da25a91e61f81be21ffe5e7ef7d0ed9e3f8de711
M66B <marcel@faircode.eu>
2016-02-20 16:32:50 +0100

    Fixed filtering enabled by default on Android 5.1
    Closes #338

--- app/src/main/java/eu/faircode/netguard/Receiver.java
+++ app/src/main/java/eu/faircode/netguard/Receiver.java
@@ -224,7 +224,7 @@ public class Receiver extends BroadcastReceiver {
                 Log.i(TAG, "Initializing sdk=" + Build.VERSION.SDK_INT);
                 editor.putBoolean("whitelist_wifi", false);
                 editor.putBoolean("whitelist_other", false);
-                if (Build.VERSION.SDK_INT == Build.VERSION_CODES.LOLLIPOP_MR1) {
+                if (Build.VERSION.SDK_INT == Build.VERSION_CODES.LOLLIPOP) {
                     editor.putBoolean("filter", true); // Optional
                     editor.putBoolean("filter_allowed", true); // Optional
                 }
