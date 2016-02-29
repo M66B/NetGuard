e006d18feb45207a93f5f8c95ca20d6627a61429
M66B <marcel@faircode.eu>
2016-02-29 14:46:37 +0100

    Fixed IPv6 VPN address

--- app/src/main/java/eu/faircode/netguard/SinkholeService.java
+++ app/src/main/java/eu/faircode/netguard/SinkholeService.java
@@ -995,7 +995,7 @@ public class SinkholeService extends VpnService implements SharedPreferences.OnS
         String vpn6 = prefs.getString("vpn6", "fd00:1:fd00:1:fd00:1:fd00:1");
         Log.i(TAG, "vpn4=" + vpn4 + " vpn6=" + vpn6);
         builder.addAddress(vpn4, 32);
-        builder.addAddress(vpn6, 64);
+        builder.addAddress(vpn6, 128);
 
         if (filter)
             for (InetAddress dns : getDns(SinkholeService.this)) {
