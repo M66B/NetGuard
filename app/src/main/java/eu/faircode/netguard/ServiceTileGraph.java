e707529560aa86d6f554cca3984caa0e77a06f8d
M66B <marcel@faircode.eu>
2016-07-22 07:59:12 +0200

    Added tile to enable/disable speed graph (Android N)

--- app/src/main/java/eu/faircode/netguard/ServiceTileGraph.java
+++ app/src/main/java/eu/faircode/netguard/ServiceTileGraph.java
@@ -21,10 +21,6 @@ package eu.faircode.netguard;
 
 
 import android.annotation.TargetApi;
-import android.app.AlarmManager;
-import android.app.PendingIntent;
-import android.content.Context;
-import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Build;
 import android.preference.PreferenceManager;
@@ -32,34 +28,24 @@ import android.service.quicksettings.Tile;
 import android.service.quicksettings.TileService;
 import android.util.Log;
 
-import java.util.Date;
-
 @TargetApi(Build.VERSION_CODES.N)
-public class ServiceTile extends TileService implements SharedPreferences.OnSharedPreferenceChangeListener {
-    private static final String TAG = "NetGuard.Tile";
-
-    public void onTileAdded() {
-        Log.i(TAG, "Added");
-    }
-
-    public void onTileRemoved() {
-        Log.i(TAG, "Removed");
-    }
+public class ServiceTileGraph extends TileService implements SharedPreferences.OnSharedPreferenceChangeListener {
+    private static final String TAG = "NetGuard.TileGraph";
 
     public void onStartListening() {
         Log.i(TAG, "Start listening");
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
         prefs.registerOnSharedPreferenceChangeListener(this);
-        boolean enabled = prefs.getBoolean("enabled", false);
-        getQsTile().setState(enabled ? Tile.STATE_ACTIVE : Tile.STATE_INACTIVE);
+        boolean stats = prefs.getBoolean("show_stats", false);
+        getQsTile().setState(stats ? Tile.STATE_ACTIVE : Tile.STATE_INACTIVE);
         getQsTile().updateTile();
     }
 
     @Override
     public void onSharedPreferenceChanged(SharedPreferences prefs, String key) {
-        if ("enabled".equals(key)) {
-            boolean enabled = prefs.getBoolean(key, false);
-            getQsTile().setState(enabled ? Tile.STATE_ACTIVE : Tile.STATE_INACTIVE);
+        if ("show_stats".equals(key)) {
+            boolean stats = prefs.getBoolean(key, false);
+            getQsTile().setState(stats ? Tile.STATE_ACTIVE : Tile.STATE_INACTIVE);
             getQsTile().updateTile();
         }
     }
@@ -73,29 +59,10 @@ public class ServiceTile extends TileService implements SharedPreferences.OnShar
     public void onClick() {
         Log.i(TAG, "Click");
 
-        // Cancel set alarm
-        AlarmManager am = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
-        PendingIntent pi = PendingIntent.getBroadcast(this, 0, new Intent(Widget.INTENT_ON), PendingIntent.FLAG_UPDATE_CURRENT);
-        am.cancel(pi);
-
         // Check state
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
-        boolean enabled = !prefs.getBoolean("enabled", false);
-        prefs.edit().putBoolean("enabled", enabled).apply();
-        if (enabled)
-            ServiceSinkhole.start("tile", this);
-        else {
-            ServiceSinkhole.stop("tile", this);
-
-            // Auto enable
-            int auto = Integer.parseInt(prefs.getString("auto_enable", "0"));
-            if (auto > 0) {
-                Log.i(TAG, "Scheduling enabled after minutes=" + auto);
-                if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M)
-                    am.set(AlarmManager.RTC_WAKEUP, new Date().getTime() + auto * 60 * 1000L, pi);
-                else
-                    am.setAndAllowWhileIdle(AlarmManager.RTC_WAKEUP, new Date().getTime() + auto * 60 * 1000L, pi);
-            }
-        }
+        boolean stats = !prefs.getBoolean("show_stats", false);
+        prefs.edit().putBoolean("show_stats", stats).apply();
+        ServiceSinkhole.reloadStats("tile", this);
     }
 }
---
+++ app/src/main/java/eu/faircode/netguard/ServiceTileMain.java
@@ -0,0 +1,93 @@
+package eu.faircode.netguard;
+
+/*
+    This file is part of NetGuard.
+
+    NetGuard is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    NetGuard is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with NetGuard.  If not, see <http://www.gnu.org/licenses/>.
+
+    Copyright 2015-2016 by Marcel Bokhorst (M66B)
+*/
+
+
+import android.annotation.TargetApi;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Build;
+import android.preference.PreferenceManager;
+import android.service.quicksettings.Tile;
+import android.service.quicksettings.TileService;
+import android.util.Log;
+
+import java.util.Date;
+
+@TargetApi(Build.VERSION_CODES.N)
+public class ServiceTileMain extends TileService implements SharedPreferences.OnSharedPreferenceChangeListener {
+    private static final String TAG = "NetGuard.TileMain";
+
+    public void onStartListening() {
+        Log.i(TAG, "Start listening");
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        prefs.registerOnSharedPreferenceChangeListener(this);
+        boolean enabled = prefs.getBoolean("enabled", false);
+        getQsTile().setState(enabled ? Tile.STATE_ACTIVE : Tile.STATE_INACTIVE);
+        getQsTile().updateTile();
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences prefs, String key) {
+        if ("enabled".equals(key)) {
+            boolean enabled = prefs.getBoolean(key, false);
+            getQsTile().setState(enabled ? Tile.STATE_ACTIVE : Tile.STATE_INACTIVE);
+            getQsTile().updateTile();
+        }
+    }
+
+    public void onStopListening() {
+        Log.i(TAG, "Stop listening");
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        prefs.unregisterOnSharedPreferenceChangeListener(this);
+    }
+
+    public void onClick() {
+        Log.i(TAG, "Click");
+
+        // Cancel set alarm
+        AlarmManager am = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+        PendingIntent pi = PendingIntent.getBroadcast(this, 0, new Intent(Widget.INTENT_ON), PendingIntent.FLAG_UPDATE_CURRENT);
+        am.cancel(pi);
+
+        // Check state
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        boolean enabled = !prefs.getBoolean("enabled", false);
+        prefs.edit().putBoolean("enabled", enabled).apply();
+        if (enabled)
+            ServiceSinkhole.start("tile", this);
+        else {
+            ServiceSinkhole.stop("tile", this);
+
+            // Auto enable
+            int auto = Integer.parseInt(prefs.getString("auto_enable", "0"));
+            if (auto > 0) {
+                Log.i(TAG, "Scheduling enabled after minutes=" + auto);
+                if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M)
+                    am.set(AlarmManager.RTC_WAKEUP, new Date().getTime() + auto * 60 * 1000L, pi);
+                else
+                    am.setAndAllowWhileIdle(AlarmManager.RTC_WAKEUP, new Date().getTime() + auto * 60 * 1000L, pi);
+            }
+        }
+    }
+}
