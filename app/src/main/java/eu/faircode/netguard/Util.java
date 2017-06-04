00e01defd2542534b2bb73a0bf73fe0c111b68f1
M66B <M66B@users.noreply.github.com>
2017-06-04 18:19:34 +0200

    Fixed getting DNS servers on Android 7.1

--- app/src/main/java/eu/faircode/netguard/Util.java
+++ app/src/main/java/eu/faircode/netguard/Util.java
@@ -275,7 +275,7 @@ public class Util {
     public static List<String> getDefaultDNS(Context context) {
         String dns1 = null;
         String dns2 = null;
-        if (android.os.Build.VERSION.SDK_INT > android.os.Build.VERSION_CODES.N) {
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.N_MR1) {
             ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
             Network an = cm.getActiveNetwork();
             if (an != null) {
