a5910a6a4154d67bda3070a840cf77184b8612d8
M66B <M66B@users.noreply.github.com>
2018-02-08 17:27:23 +0100

    Disable Xposed check

--- app/src/main/java/eu/faircode/netguard/Util.java
+++ app/src/main/java/eu/faircode/netguard/Util.java
@@ -429,7 +429,7 @@ public class Util {
     }
 
     public static boolean hasXposed(Context context) {
-        if (!isPlayStoreInstall(context))
+        if (true || !isPlayStoreInstall(context))
             return false;
         for (StackTraceElement ste : Thread.currentThread().getStackTrace())
             if (ste.getClassName().startsWith("de.robv.android.xposed"))
