274755c1ff11cce2be684e80cda4e7ca7de3daa3
DrunkenSasquatch <j.hamshere91@gmail.com>
2018-04-20 02:07:16 +1000

    update lookup domain name

--- app/src/main/java/eu/faircode/netguard/ActivityLog.java
+++ app/src/main/java/eu/faircode/netguard/ActivityLog.java
@@ -195,7 +195,7 @@ public class ActivityLog extends AppCompatActivity implements SharedPreferences.
                 popup.getMenu().findItem(R.id.menu_protocol).setTitle(Util.getProtocolName(protocol, version, false));
 
                 // Whois
-                final Intent lookupIP = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.tcpiputils.com/whois-lookup/" + ip));
+                final Intent lookupIP = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.dnslytics.com/whois-lookup/" + ip));
                 if (pm.resolveActivity(lookupIP, 0) == null)
                     popup.getMenu().removeItem(R.id.menu_whois);
                 else
