876f1eebd66d6973afc3e9741d44dc4fbad66ae1
M66B <M66B@users.noreply.github.com>
2019-01-09 19:14:33 +0000

    Revert "Fix condition for filtering local DNS servers"
    This reverts commit 901cfcce9d8cd56d63ce1a28b529aea5aae61989.

--- app/src/main/java/eu/faircode/netguard/ServiceSinkhole.java
+++ app/src/main/java/eu/faircode/netguard/ServiceSinkhole.java
@@ -1105,7 +1105,7 @@ public class ServiceSinkhole extends VpnService implements SharedPreferences.OnS
         // Remove local DNS servers when not routing LAN
         boolean lan = prefs.getBoolean("lan", false);
         boolean use_hosts = prefs.getBoolean("filter", false) && prefs.getBoolean("use_hosts", false);
-        if (!lan && use_hosts) {
+        if (lan && use_hosts) {
             List<InetAddress> listLocal = new ArrayList<>();
             try {
                 Enumeration<NetworkInterface> nis = NetworkInterface.getNetworkInterfaces();
