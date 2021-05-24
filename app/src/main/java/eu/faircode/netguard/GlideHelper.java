55cba9fd213f894455280595d1bf214b6dc8ed8f
Prasoon Kumar <prasoonk187@gmail.com>
2021-05-24 14:43:05 +0530

    1. :rocket: Added kotlin support to the project. 2. :fire: Activities and adapters has been converted to kotlin. 3. :rocket: Deprecated AsyncTask is replaced by Kotlin CoroutineAsyncTask.

--- app/src/main/java/eu/faircode/netguard/GlideHelper.java
+++
@@ -1,8 +0,0 @@
-package eu.faircode.netguard;
-
-import com.bumptech.glide.annotation.GlideModule;
-import com.bumptech.glide.module.AppGlideModule;
-
-@GlideModule
-public final class GlideHelper extends AppGlideModule {
-}
